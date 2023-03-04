32381ace8733d6747e6460dffd0ea3de9c7bbd69
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2023-03-04 22:34:58 +1100

    Update backend port

--- backend/core/src/main/java/cabbage/missingwebtracker/backend/core/BackendApplication.java
+++ backend/core/src/main/java/cabbage/missingwebtracker/backend/core/BackendApplication.java
@@ -1,22 +1,13 @@
 package cabbage.missingwebtracker.backend.core;
 
-import cabbage.missingwebtracker.backend.core.database.MemoryMissingReportDatabase;
-import cabbage.missingwebtracker.backend.core.report.MissingReport;
-import cabbage.missingwebtracker.backend.core.report.MissingReportBaseBuilder;
-import cabbage.missingwebtracker.backend.core.report.PersonExtension;
-import cabbage.missingwebtracker.backend.core.report.PetExtension;
-import cabbage.missingwebtracker.backend.core.report.ReportSourceType;
-import cabbage.missingwebtracker.backend.core.report.ReportType;
-import cabbage.missingwebtracker.backend.core.util.Gender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
+        System.setProperty("server.port", "9999");
         SpringApplication.run(BackendApplication.class, args);
     }
 
