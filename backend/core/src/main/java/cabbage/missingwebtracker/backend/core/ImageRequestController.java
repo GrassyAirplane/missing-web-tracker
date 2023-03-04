84b7ee3083db596c841f8226ecd11898581384c5
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2023-03-04 22:55:51 +1100

    Add cors on the image controller

--- backend/core/src/main/java/cabbage/missingwebtracker/backend/core/ImageRequestController.java
+++ backend/core/src/main/java/cabbage/missingwebtracker/backend/core/ImageRequestController.java
@@ -6,6 +6,7 @@ import cabbage.missingwebtracker.backend.core.database.MemoryMissingReportDataba
 import cabbage.missingwebtracker.backend.core.util.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 public class ImageRequestController {
     @Autowired
     private MemoryMissingReportDatabase reportDatabase;
