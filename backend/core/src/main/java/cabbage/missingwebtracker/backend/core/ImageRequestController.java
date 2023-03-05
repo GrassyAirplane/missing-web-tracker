bc69a9a104850590d6f966e6c6fa0d83423ebfb0
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2023-03-05 21:22:53 +1100

    disable cors

--- backend/core/src/main/java/cabbage/missingwebtracker/backend/core/ImageRequestController.java
+++ backend/core/src/main/java/cabbage/missingwebtracker/backend/core/ImageRequestController.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:8080", "http://localhost:5173"})
+//@CrossOrigin(origins = {"http://localhost:8080", "http://localhost:5173"})
 public class ImageRequestController {
     @Autowired
     private MemoryMissingReportDatabase reportDatabase;
