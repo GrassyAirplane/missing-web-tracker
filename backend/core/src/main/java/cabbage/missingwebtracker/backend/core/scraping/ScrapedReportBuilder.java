fed39e90ac968a0b016d67c21253f1aa1d21f91b
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2023-03-05 18:30:38 +1100

    Fix scraper not adding gender

--- backend/core/src/main/java/cabbage/missingwebtracker/backend/core/scraping/ScrapedReportBuilder.java
+++ backend/core/src/main/java/cabbage/missingwebtracker/backend/core/scraping/ScrapedReportBuilder.java
@@ -1,11 +1,16 @@
 package cabbage.missingwebtracker.backend.core.scraping;
 
+import cabbage.missingwebtracker.backend.core.util.Gender;
+
 public final class ScrapedReportBuilder {
     private String name;
     private int age;
 
     private long lastSeenEpochMilli;
     private String lastKnownLocation;
+
+    private Gender gender;
+
     public ScrapedReportBuilder name(String name) {
         this.name = name;
         return this;
@@ -26,10 +31,15 @@ public final class ScrapedReportBuilder {
         return this;
     }
 
+    public ScrapedReportBuilder gender(Gender gender) {
+        this.gender = gender;
+        return this;
+    }
+
     public ScrapedReport build() {
         if (name == null || age == 0 || lastSeenEpochMilli == 0 || lastKnownLocation == null) {
             return null;
         }
-        return new ScrapedReport(name, age, lastSeenEpochMilli, lastKnownLocation);
+        return new ScrapedReport(name, age, lastSeenEpochMilli, lastKnownLocation, gender);
     }
 }
